9851bdb699aa32fbcd70e93d1d0f541400608b85
ur5fot <ur5fot@gmail.com>
2026-01-04 16:30:12 +0200

    Update env config

--- src/config/env.js
+++ src/config/env.js
@@ -7,6 +7,6 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   tronGridEndpoint: process.env.TRON_GRID_ENDPOINT || 'https://api.trongrid.io', // mainnet
   // tronGridEndpoint: process.env.TRON_GRID_ENDPOINT || 'https://nile.trongrid.io', // testnet Nile
-  tronGridApiKey: process.env.TRON_GRID_API_KEY || 'cb71f17f-cb0b-4af4-b1c9-a547276dbf18', // demo key
+  tronGridApiKey: process.env.TRON_GRID_API_KEY , 
   privateKey: process.env.PRIVATE_KEY || '01', // demo key
 }
